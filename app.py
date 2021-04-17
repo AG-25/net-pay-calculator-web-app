a174b87b280b33700fd985127a1de699b1001aa4
Adam Goddard <adam_goddard1@outlook.com>
2021-04-17 20:55:46 +0100

    Initial: set up project structure

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template("index.html")
