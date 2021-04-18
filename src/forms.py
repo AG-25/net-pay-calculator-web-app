c25e52dca28e8fc8dc7f762a97ff5d4dc9aad81b
Adam Goddard <adam_goddard1@outlook.com>
2021-04-18 15:49:48 +0100

    Fix: correct form validation for inputs that are -ve or zero

--- src/forms.py
+++ src/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, FloatField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired, NumberRange
 
 
 class EmployeeSalaryForm(FlaskForm):
-    salary = FloatField("Annual Salary (GBP):", validators=[DataRequired()])
+    salary = FloatField(
+        "Annual Salary (GBP):", validators=[InputRequired(), NumberRange(min=0)]
+    )
     submit = SubmitField("Calculate")
