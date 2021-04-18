170e618f207027a8e013bd43ab1d0f730d3b6516
Adam Goddard <adam_goddard1@outlook.com>
2021-04-18 11:02:46 +0100

    Feat: add javascript to dynamically populate dates and keep track of a users scroll position

---
+++ static/js/index.js
@@ -0,0 +1,19 @@
+// Dynamic date setting, to include the current UK tax year in the homepage
+const today = new Date;
+const year = today.getFullYear();
+if (today.getMonth() < 3) {
+    $(".tax-year").text(`${year-1}/${year}`);
+} else {
+    $(".tax-year").text(`${year}/${year+1}`);
+};
+
+// Keeps track of the users scroll position (e.g. when form submission occurs)
+$(window).scroll(function() {
+sessionStorage.scrollTop = $(this).scrollTop();
+});
+
+$(document).ready(function() {
+  if (sessionStorage.scrollTop != "undefined") {
+    $(window).scrollTop(sessionStorage.scrollTop);
+  }
+});
